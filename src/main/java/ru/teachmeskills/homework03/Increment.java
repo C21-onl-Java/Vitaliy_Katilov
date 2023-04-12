5e071436faecb7e0fc91ba56df0461c70739132b
Vitaliy-Katilov <vitaliy.ka-75@yandex.ru>
2023-04-13 01:02:50 +0300

    homework 3 cycles ex 1

---
+++ src/main/java/ru/teachmeskills/homework03/Increment.java
@@ -0,0 +1,10 @@
+package ru.teachmeskills.homework03;
+
+public class Increment {
+    public static void main(String[] args) {
+        final int MAX = 99;
+        for (int i = 1; i <= MAX; ++i) {
+            System.out.println(i);
+        }
+    }
+}
