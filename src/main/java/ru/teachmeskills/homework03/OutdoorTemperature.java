8bfbdbaba190fdbe18bffc81d90ea5a4b92f8f7c
Vitaliy-Katilov <vitaliy.ka-75@yandex.ru>
2023-04-12 23:49:53 +0300

    homework 3 exercise 4

---
+++ src/main/java/ru/teachmeskills/homework03/OutdoorTemperature.java
@@ -0,0 +1,22 @@
+package ru.teachmeskills.homework03;
+
+import java.util.Scanner;
+
+public class OutdoorTemperature {
+    public static void main(String[] args) {
+        System.out.print("Enter outdoor temperature: ");
+        Scanner scanner = new Scanner(System.in);
+        int  temperature  = scanner.nextInt();
+
+        if (temperature > -5) {
+            System.out.println("Warm.");
+        }
+        else if (temperature <= -5 && temperature >= -20) {
+            System.out.println("Fine.");
+        }
+        else
+            System.out.println("Cold.");
+
+    }
+
+}
