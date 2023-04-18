d23d53dac11510a28ea04b4a672dbb20a5e0c7c7
Vitaliy-Katilov <vitaliy.ka-75@yandex.ru>
2023-04-18 23:38:26 +0300

    homework 3 additional tasks ex 2 corrected

--- src/main/java/ru/teachmeskills/homework03/BankDeposit.java
+++ src/main/java/ru/teachmeskills/homework03/BankDeposit.java
@@ -4,17 +4,16 @@ import java.util.Scanner;
 
 public class BankDeposit {
     public static void main(String[] args) {
-        final float PERCENT_OF_MONTH = 7;
+        final float PERCENT = 7f/100;
         System.out.print("Enter deposit amount: ");
         Scanner scanner = new Scanner(System.in);
         float initDeposit = scanner.nextFloat();
         System.out.print("Enter number of months: ");
         int numberMonth = scanner.nextInt();
-        float finalDeposit = initDeposit;
 
         for (int i = 0; i < numberMonth; i++) {
-            finalDeposit += finalDeposit * (PERCENT_OF_MONTH / 100);
+            initDeposit += initDeposit * PERCENT;
         }
-        System.out.println("The final amount of the deposit for " + numberMonth + " months is " + finalDeposit);
+        System.out.println("The final amount of the deposit for " + numberMonth + " months is " + initDeposit);
     }
 }
